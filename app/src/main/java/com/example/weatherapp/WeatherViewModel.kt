180b707f94ad61083465cfc1bcb900b64d86e554
ilya <ilyanosevich@gmail.com>
2024-11-27 23:35:23 +0300

    добавление местоположения во ViewModel, добавление обновления проггоза по часам и по дням

--- app/src/main/java/com/example/weatherapp/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherapp/WeatherViewModel.kt
@@ -5,23 +5,47 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class WeatherViewModel : ViewModel() {
+
+    private val _locationviewer = MutableLiveData<LocationViewer>()
+    val locationViewer: LiveData<LocationViewer?> = _locationviewer
+
     private val _currentweather = MutableLiveData<CurrentWeather?>()
     val weather: LiveData<CurrentWeather?> = _currentweather
 
-    private val _hourlyforecast = MutableLiveData<HourlyForecast>()
+    private val _hourlyforecast = MutableLiveData<HourlyForecast?>()
     val hourlyForecast: LiveData<HourlyForecast?> = _hourlyforecast
 
     private val _dailyforecast = MutableLiveData<Daily>()
     val dailyForecast: LiveData<Daily?> = _dailyforecast
-    fun fetchWeather(latitude: Double, longitude: Double) {
+    val openMeteoAPI = OpenMeteoAPI()
+
+    fun fetchWeather() {
         viewModelScope.launch {
-            val openMeteoAPI = OpenMeteoAPI()
-            val currentWeather = openMeteoAPI.getCurrentWeather(latitude, longitude)
-            val hourlyForecast = openMeteoAPI.getHourlyForecast(latitude, longitude)
+
+            locationViewer.value?.getLastKnownLocation()
+            val currentWeather = locationViewer.value?.let {
+                openMeteoAPI.getCurrentWeather(
+                    it.latitude, it.longitude)
+            }
+            val hourlyForecast = locationViewer.value?.let {
+                openMeteoAPI.getHourlyForecast(
+                    it.latitude, it.longitude)
+            }
             _currentweather.postValue(currentWeather) // Обновляем данные
+            _hourlyforecast.postValue(hourlyForecast)
+        }
+    }
+
+    private fun startWeatherUpdateLoop() {
+        viewModelScope.launch {
+            while (true) {
+                fetchWeather() // Пример координат
+                delay(30 * 60 * 1000L) // 30 минут в миллисекундах
+            }
         }
     }
 }
